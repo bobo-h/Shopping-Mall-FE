bff37a89049e47b00e5a12ec9f9532d1ff794692
bobo-h <166573095+bobo-h@users.noreply.github.com>
2024-10-29 23:54:21 +0900

    feat: admin 페이지 네이션 로직 추가

--- src/features/product/productSlice.js
+++ src/features/product/productSlice.js
@@ -7,9 +7,10 @@ export const getProductList = createAsyncThunk(
   "products/getProductList",
   async (query, { rejectWithValue }) => {
     try {
-      const response = await api.get("/product");
+      const response = await api.get("/product", { params: { ...query } });
+      console.log("getProductResponse", response);
       if (response.status !== 200) throw new Error(response.error);
-      return response.data.data;
+      return response.data;
     } catch (error) {
       return rejectWithValue(error.error);
     }
@@ -93,8 +94,9 @@ const productSlice = createSlice({
       })
       .addCase(getProductList.fulfilled, (state, action) => {
         state.loading = false;
-        state.productList = action.payload;
+        state.productList = action.payload.data;
         state.error = "";
+        state.totalPageNum = action.payload.totalPageNum;
       })
       .addCase(getProductList.rejected, (state, action) => {
         state.loading = false;
--- src/page/AdminProductPage/AdminProductPage.js
+++ src/page/AdminProductPage/AdminProductPage.js
@@ -38,11 +38,18 @@ const AdminProductPage = () => {
 
   // 상품리스트 가져오기 (url쿼리 맞춰서)
   useEffect(() => {
-    dispatch(getProductList());
-  }, []);
+    dispatch(getProductList({ ...searchQuery }));
+  }, [query]);
 
   useEffect(() => {
     // 검색어나 페이지가 바뀌면 url바꿔주기 (검색어또는 페이지가 바뀜 => url 바꿔줌=> url쿼리 읽어옴=> 이 쿼리값 맞춰서  상품리스트 가져오기)
+    if (searchQuery.name === "") {
+      delete searchQuery.name;
+    }
+    const params = new URLSearchParams(searchQuery);
+    const query = params.toString();
+    console.log("query", query);
+    navigate("?" + query);
   }, [searchQuery]);
 
   const deleteItem = (id) => {
@@ -63,7 +70,12 @@ const AdminProductPage = () => {
 
   const handlePageClick = ({ selected }) => {
     // 쿼리에 페이지값 바꿔주기
+    setSearchQuery({ ...searchQuery, page: selected + 1 });
   };
+  // 검색 매커니즘
+  // searchBox에서 검색어를 읽어온다 => 엔터를 치면 => searchQuery객체가 업데이트가 된다. ex:{name: jacket}
+  // => searchQuery 객체 안의 아이템 기준으로 url을 새로 생성해서 호출 ex: $name=jacket
+  // => url쿼리 읽어오기 => 쿼리 기준으로 BE 검색 조건과 함께 호출
 
   return (
     <div className="locate-center">
@@ -90,7 +102,7 @@ const AdminProductPage = () => {
           nextLabel="next >"
           onPageChange={handlePageClick}
           pageRangeDisplayed={5}
-          pageCount={100}
+          pageCount={totalPageNum}
           forcePage={searchQuery.page - 1}
           previousLabel="< previous"
           renderOnZeroPageCount={null}
