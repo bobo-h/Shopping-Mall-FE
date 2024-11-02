8e617b5bd9e2c49c893a4f21fdf225cd7d76470b
bobo-h <166573095+bobo-h@users.noreply.github.com>
2024-11-02 23:06:44 +0900

    feat: add loading spinner to cartPage

--- src/page/CartPage/CartPage.js
+++ src/page/CartPage/CartPage.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { useEffect } from "react";
-import { Container, Row, Col } from "react-bootstrap";
+import { Container, Row, Col, Spinner } from "react-bootstrap";
 import { useDispatch, useSelector } from "react-redux";
 import CartProductCard from "./component/CartProductCard";
 import OrderReceipt from "../PaymentPage/component/OrderReceipt";
@@ -9,7 +9,7 @@ import { getCartList } from "../../features/cart/cartSlice";
 
 const CartPage = () => {
   const dispatch = useDispatch();
-  const { cartList, totalPrice } = useSelector((state) => state.cart);
+  const { cartList, totalPrice, loading } = useSelector((state) => state.cart);
 
   useEffect(() => {
     //카트리스트 불러오기
@@ -18,23 +18,35 @@ const CartPage = () => {
 
   return (
     <Container>
-      <Row>
-        <Col xs={12} md={7}>
-          {cartList.length > 0 ? (
-            cartList.map((item) => (
-              <CartProductCard item={item} key={item._id} />
-            ))
-          ) : (
-            <div className="text-align-center empty-bag">
-              <h2>카트가 비어있습니다.</h2>
-              <div>상품을 담아주세요!</div>
-            </div>
-          )}
-        </Col>
-        <Col xs={12} md={5}>
-          <OrderReceipt cartList={cartList} totalPrice={totalPrice} />
-        </Col>
-      </Row>
+      {loading ? (
+        <div className="text-center w-100 my-5">
+          <Spinner
+            animation="border"
+            role="status"
+            style={{ width: "4rem", height: "4rem", color: "#1abc9c" }}
+          >
+            <span className="visually-hidden">Loading...</span>
+          </Spinner>
+        </div>
+      ) : (
+        <Row>
+          <Col xs={12} md={7}>
+            {cartList.length > 0 ? (
+              cartList.map((item) => (
+                <CartProductCard item={item} key={item._id} />
+              ))
+            ) : (
+              <div className="text-align-center empty-bag">
+                <h2>카트가 비어있습니다.</h2>
+                <div>상품을 담아주세요!</div>
+              </div>
+            )}
+          </Col>
+          <Col xs={12} md={5}>
+            <OrderReceipt cartList={cartList} totalPrice={totalPrice} />
+          </Col>
+        </Row>
+      )}
     </Container>
   );
 };
